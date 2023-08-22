b19005dee6cccf789dc273a3b7ecf7fd4577c1c0
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-08-22 13:19:06 +0200

    resolve exam week 3

---
+++ Semana3-Bucles/examen.py
@@ -0,0 +1,12 @@
+#for n in range (6, 18+1, 3):
+#    print(n*2)
+
+#for n in range(19):
+#   if n % 2 == 0:
+#       print(n)
+
+#for x in range(1, 10+1):
+#   print(x**3)
+
+for n in range (0, 100, 7*7):
+    print(n)
