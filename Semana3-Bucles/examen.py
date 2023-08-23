ff05f58d12fdcb6f65b505e1d14ed5fcc351fae3
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-08-23 10:29:25 +0200

    resolve exam week 3

--- Semana3-Bucles/examen.py
+++ Semana3-Bucles/examen.py
@@ -1,12 +1,21 @@
-#for n in range (6, 18+1, 3):
-#    print(n*2)
+#Imprimir los numeros 12,18,24,30,36
+for n in range (6, 18+1, 3):
+    print(n*2)
 
-#for n in range(19):
-#   if n % 2 == 0:
-#       print(n)
+#Bucles que imprimen los numeos pares del 0 al 18
+for n in range(19):
+   if n % 2 == 0:
+      print(n)
 
-#for x in range(1, 10+1):
-#   print(x**3)
+for n in range(10):
+    print(n+n) #Equivalente a la expresion(n*2)
 
-for n in range (0, 100, 7*7):
-    print(n)
+#Imprime los primeros 10 numeros elevados al cubo(x**3) donde x=1 con x=10
+for x in range(1, 10+1):
+   print(x**3)
+
+#Múltiplos de 7 entre 0 a 100
+for num in range(0, 100+1, 7):
+    print(num)
+
+
