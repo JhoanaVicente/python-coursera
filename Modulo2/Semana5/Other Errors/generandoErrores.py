11914a425520f4cb60c343941bd11716c44e9897
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-09-28 09:01:30 +0200

    Semana 5: Errors and Exceptions

---
+++ Modulo2/Semana5/Other Errors/generandoErrores.py
@@ -0,0 +1,17 @@
+def validate_user(username, minlen):
+    assert type(username) == str, "username must be a string"
+    if minlen < 1:
+        raise ValueError("minlen must be at least 1")
+    if len(username) < minlen:
+        return False
+    if not username.isalnum():
+        return False
+    return True
+
+
+print(validate_user("", 1))
+print(validate_user("myuser", 1))
+print(validate_user(88, 1))
+print(validate_user([], 1))
+print(validate_user(["name"], 1))
+print(validate_user([3], 1))
